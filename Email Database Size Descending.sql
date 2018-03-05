a2ee142d9af03503502f0572ddda59a20a9868ed
Francesco Mantovani <3397477+francesco1119@users.noreply.github.com>
2018-03-06 11:06:06 +1300

    Update Email Database Size Descending.sql

--- Email Database Size Descending.sql
+++ Email Database Size Descending.sql
@@ -29,7 +29,9 @@ SET @xml = CAST((	SELECT
 					FOR XML PATH('tr'), ELEMENTS ) AS NVARCHAR(MAX))
 
 
-SET @body ='<html><body><H3>Database Rank by DB size Descending</H3>
+SET @body ='<html><body><H2>Database Rank by DB size Descending</H2>
+			<H3>This e-mail lists all databases on the server by size descending</H3>
+			<H3>Please delete all databases that are no longer in use</H3>
 			<table border = 1> 
 			<tr>
 			<th>Name</th> <th>DB_Size_Descending</th> <th>owner</th> <th>db_id</th> <th>created</th> <th>status</th> <th>compatibility_level</th></tr>'    
@@ -39,10 +41,10 @@ SET @body = @body + @xml +'</table></body></html>'
 
 
 EXEC msdb.dbo.sp_send_dbmail
-@profile_name = 'MyEmailProfile', -- replace with your SQL Database Mail Profile 
+@profile_name = 'MonthlySQlSpringClean', -- replace with your SQL Database Mail Profile 
 @body = @body,
 @body_format ='HTML',
-@recipients = 'myemail@domain.net', -- replace with your email address
+@recipients = 'YourEmail', -- replace with your email address
 @subject = 'Database Rank by DB size Descending' ;
 
 
