cb732ff33528b3878412b61b9dc7f0612e0c74be
Francesco Mantovani <3397477+francesco1119@users.noreply.github.com>
2018-03-05 22:22:50 +1300

    Update Email Database Date Ascending.sql

--- Email Database Date Ascending.sql
+++ Email Database Date Ascending.sql
@@ -29,7 +29,7 @@ SET @xml = CAST((	SELECT
 					FOR XML PATH('tr'), ELEMENTS ) AS NVARCHAR(MAX))
 
 
-SET @body ='<html><body><H3>Database Rank by Creation_Date_Ascending</H3>
+SET @body ='<html><body><H3>Database Rank by Creation Date Ascending</H3>
 			<table border = 1> 
 			<tr>
 			<th>Name</th> <th>db_size</th> <th>owner</th> <th>db_id</th> <th>Creation_Date_Ascending</th> <th>status</th> <th>compatibility_level</th></tr>'    
@@ -39,13 +39,11 @@ SET @body = @body + @xml +'</table></body></html>'
 
 
 EXEC msdb.dbo.sp_send_dbmail
-@profile_name = 'FrancescoEmailProfile', -- replace with your SQL Database Mail Profile 
+@profile_name = 'MyEmailProfile', -- replace with your SQL Database Mail Profile 
 @body = @body,
 @body_format ='HTML',
-@recipients = 'francesco84mantovani@gmail.com', -- replace with your email address
+@recipients = 'myemail@domain.net', -- replace with your email address
 @subject = 'Database Rank by DB size Descending' ;
 
 
 DROP TABLE #Creation_Date_Ascending
-
---select Product_strCode,  Product_strVersion,  Product_strBuild  from VISTAMikeL.dbo.tblProduct
