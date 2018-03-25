3df98a5654484b853a6b9db1b2403445c65ece6c
Francesco Mantovani <3397477+francesco1119@users.noreply.github.com>
2018-03-25 22:13:40 +1300

    Update I_Made_Bubu.sql

--- I_Made_Bubu.sql
+++ I_Made_Bubu.sql
@@ -7,5 +7,9 @@ EXECUTE msdb.dbo.sysmail_delete_account_sp
     @account_name = 'SQL_Spring_Cleaning_Account' ;  
 
 EXEC sp_delete_job  
-    @job_name = N'Email_Date_Ascending' ;  
+    @job_name = N'Database Rank by DB Date Ascending' ;  
+
+EXEC sp_delete_job  
+    @job_name = N'Database Rank by DB size Descending' ;  
+  
   
